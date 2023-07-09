5b8ba08676e78e2174db10769e48d2939636c522
Sakisakthi <sakthiask95@gmail.com>
2023-07-09 21:54:05 +0530

    button
    button

--- js.js
+++ js.js
@@ -23,10 +23,84 @@ edt.addEventListener("click", function() {
   place.contentEditable = true;
   sav1.style.display="block";
   edt.style.display="none";
-
+} );
+sav1.addEventListener("click", function() {
+  paragraph.contentEditable = false;
+  lead.contentEditable = false;
+  place.contentEditable = false;
+  sav1.style.display = "none";
+  edt.style.display = "block";
 
 } );
 
-sav1.addEventListener("click", function() {
-  paragraph2.contentEditable = false;
-} )
+
+
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const mail = document.getElementById("email");
+const phone = document.getElementById("phn");
+const bio = document.getElementById("bio");
+
+const edtbtn = document.getElementById("edtbtn1");
+const savbtn = document.getElementById("savbtn1");
+savbtn.style.display="none";
+
+edtbtn.addEventListener("click", function(){
+  fname.contentEditable = true;
+  lname.contentEditable = true;
+  mail.contentEditable = true;
+  phone.contentEditable = true;
+  bio.contentEditable = true;
+   
+  savbtn.style.display = "block";
+  edtbtn.style.display = "none";
+});
+savbtn.addEventListener("click", function() 
+{
+  fname.contentEditable = false;
+  lname.contentEditable = false;
+  mail.contentEditable = false;
+  phone.contentEditable = false;
+  bio.contentEditable = false;
+  savbtn.style.display = "none";
+  edtbtn.style.display = "block";
+
+});
+
+
+
+
+
+const countryplace = document.getElementById ("country1");
+const  stateplace = document.getElementById("state");
+const  statepincode = document.getElementById("pincode");
+const  staxid = document.getElementById("taxid");
+
+const editbutton3 = document.getElementById("editbutton3");
+const savebutton3 = document.getElementById("savebutton3");
+savebutton3.style.display = "none";
+
+editbutton3.addEventListener("click", function()
+{
+  countryplace.contentEditable = true;
+  stateplace.contentEditable = true;
+  statepincode.contentEditable = true;
+  staxid.contentEditable = true;
+  
+  editbutton3.style.display = "none";
+  savebutton3.style.display = "block";
+
+});
+
+savebutton3.addEventListener("click",function()
+{
+  countryplace.contentEditable = false;
+  stateplace.contentEditable = false;
+  statepincode.contentEditable = false;
+  staxid.contentEditable = false;
+
+  editbutton3.style.display ="block";
+  savebutton3.style.display = "none"
+});
+
+
